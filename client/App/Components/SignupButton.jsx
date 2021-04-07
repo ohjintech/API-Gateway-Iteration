5494847361ff900a4ef2af1cb268d7a0e7c8b236
Jin Oh <jintoh613@gmail.com>
2021-04-07 15:14:32 -0700

    Got the modals and form working, included the complete button on the modal but still needs to cancel button

--- client/App/Components/SignupButton.jsx
+++ client/App/Components/SignupButton.jsx
@@ -3,31 +3,50 @@ import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 // const [ details, setDetails ] = useState({});
 
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
 const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(2),
+      width: '25ch',
+    },
+  },
   modal: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
   paper: {
+    position: 'absolute',
+    width: 450,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: '1px solid #7BDFF2',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
-
-
 const SignupButton = () => {
 
-  const [showModal, setShow] = useState(true);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [showModal, setShow] = useState(false);
   
   const handleOpen = () => {
     setShow(true);
@@ -37,6 +56,25 @@ const SignupButton = () => {
     setShow(false);
   };
     
+
+  const signUpModalBody = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="signup-modal-title">Sign Up</h2>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+        <TextField required id="outlined-basic" label="Username" variant="outlined" />
+        <TextField required id="outlined-basic" label="Password" type="password" autoComplete="current-password" variant="outlined" />
+        </div>
+        <div>
+        <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+        <TextField id="outlined-basic" label="SSN" variant="outlined" helperText="* Required"/>
+        </div>
+
+        <Button variant="contained" color="primary">Complete</Button>
+      </form>
+    </div>
+  );
+
     return(
       <div>
         <Button onClick={handleOpen} variant="outlined" color="primary">
@@ -44,15 +82,13 @@ const SignupButton = () => {
         </Button>
 
 
-        <Modal style = {{display: 'flex', alignContent: 'center', justifyContent: 'center', height: '50em'}}
+        <Modal
           aria-labelledby="transition-modal-title"
           aria-describedby="transition-modal-description"
           open={showModal}
           onClose={handleClose}
         >
-          <div className="modalStyle" style={{maxWidth: '50em'}}>
-            hello
-          </div>
+          {signUpModalBody}
         </Modal>
       </div>
 
